cef3fc29c9b28f5cc84d6025265ef7abd7c156a2
Eduardo A. Maroto Campos <ultracodez@outlook.com>
2025-01-23 21:41:31 -0800

    estimated arrival times + styling

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -6,6 +6,7 @@
     let isRunning: boolean | null = null;
     let details: string = "";
     let next: string = "";
+    let singleTrainTimes: Array<any>= [];
 
     let indices = {
         'Metro D Line - Wilshire / Western Station': [
@@ -63,6 +64,8 @@
     }
 
     async function fetchIsRunning() {
+
+        // initial lookup
         const response = await fetch(
             "https://birch.catenarymaps.org/get_realtime_locations/metro~losangeles/metro/0/0",
         );
@@ -70,7 +73,26 @@
         const hr4ktrips: any = Object.values(data.vehicle_positions).filter((trip: any) => (trip && trip.vehicle && trip.vehicle.id && trip.vehicle.id.startsWith("40") && trip.vehicle.id.length > 4));
         console.log(hr4ktrips);
         if (hr4ktrips.length > 0) {
+            // extra train-specific data collection
             const hr4ktrip = hr4ktrips[0]
+            console.log(hr4ktrip);
+            const tDataResponse = await fetch(`https://birch_req_trip.catenarymaps.org/get_trip_information/metro~losangeles/?trip_id=${hr4ktrip.trip.trip_id}&start_date=${hr4ktrip.trip.start_date}`)
+            const tData = await tDataResponse.json();
+            console.log("trainset",hr4ktrip.vehicle.id,"",tData)
+
+            // info display
+
+            if(tData && tData.stoptimes)
+                tData.stoptimes.map((stoptime:any)=>{
+                    console.log(stoptime)
+                    const tDate = stoptime?.rt_arrival?.time && new Date(stoptime?.rt_arrival?.time * 1000)
+
+                    if(!!tDate)
+                        singleTrainTimes = singleTrainTimes.concat({name: stoptime.name, time: `${tDate.getHours() % 12 || 12}:${tDate.getMinutes()}`})
+            })
+
+            console.log(singleTrainTimes)
+
             isRunning = true;
             details = `HR4000 set ${hr4ktrip.vehicle.id} is running on the ${hr4ktrip.trip.trip_headsign.replace(' - ', ' to ').replace(' Station', '').replace(' Union', ' Union Station')}.`;
             // @ts-ignore
@@ -98,8 +120,18 @@
             <h2 style="font-size:4rem;">Loading...</h2>
             <h3 style="font-size: 2rem;">----</h3>
         {/if}
+                        {#if isRunning == true}
         <p style="font-size: 1.5rem;">{details}<br />{next}</p>
-        <br />
+        
+
+        <h3 style="font-size: 2rem;">Estimated Arrival Times</h3>
+        {#each singleTrainTimes as singleTrainTime}
+	        <div style="font-size:1.5rem;  padding: 2px 30px;">
+		       {singleTrainTime.name} @ {singleTrainTime.time}
+	        </div>
+        {/each}
+                {/if}
+        <br/>
         <a href="https://samuelsharp.com" style="font-size: 1.25rem;color:white;">Built by Samuel Sharp (@42A7C5)</a>
     </div>
 </div>
@@ -109,11 +141,12 @@
         display: flex;
         justify-content: center;
         align-items: center;
-        height: 100vh;
+        min-height: 100vh;
     }
 
     .wrap div {
         text-align: center;
         padding: 30px;
+
     }
 </style>
